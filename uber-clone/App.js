3a9193d15cec295b669714f48300a4b0bc863097
karankashyap04 <karankashyap@gmail.com>
2022-11-21 04:34:32 -0500

    feat: Start HomeScreen component: add Uber logo

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -2,15 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     // setting up data layer with store (for Redux)
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>UBER</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ uber-clone/screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, SafeAreaView, Image, StyleSheet } from "react-native";
+import tw from "twrnc";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={styles.mainLogo}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/800px-Uber_logo_2018.svg.png?20180914002846",
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainLogo: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+  },
+});
