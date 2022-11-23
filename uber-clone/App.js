83dab32866922ae87c4a84905da2fd1d44b11909
karankashyap04 <karankashyap@gmail.com>
2022-11-22 22:47:52 -0500

    feat: Add Navigation functionality for MapScreen and EatsScreen

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -4,14 +4,38 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import EatsScreen from "./screens/EatsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     // setting up data layer with store (for Redux)
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="EatsScreen"
+              component={EatsScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- uber-clone/components/NavigationOptions.js
+++ uber-clone/components/NavigationOptions.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 import tw from "twrnc";
 
 const data = [
@@ -19,13 +20,18 @@ const data = [
 ];
 
 export default function NavigationOptions() {
+  const navigation = useNavigation();
+
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.key}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 w-40 m-2`}>
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 w-40 m-2`}
+          onPress={() => navigation.navigate(item.screen)}
+        >
           <View>
             <Image
               source={{ uri: item.image }}
